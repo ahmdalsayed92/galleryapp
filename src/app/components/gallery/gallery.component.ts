e757f83da0ff3e8008912f7776d4f25600d9946f
ahmdalsayed92 <ahmdalsayed92@gmail.com>
2022-03-05 17:40:22 +0200

    - adding image title - adding code comments

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.scss'],
 })
 export class GalleryComponent implements OnInit {
+  /**
+   *An array of objects to contain the whole images
+   * @memberof GalleryComponent
+   */
   images = [
     { url: 'https://api.lorem.space/image/game?w=600&h=750' },
     { url: 'https://api.lorem.space/image/game?w=750&h=600' },
@@ -41,15 +45,30 @@ export class GalleryComponent implements OnInit {
     { url: 'https://api.lorem.space/image/game?w=600&h=750' },
     { url: 'https://api.lorem.space/image/game?w=600&h=750' },
   ];
+
+  /**
+   * Triger flag for the overlay background and close button
+   * @memberof GalleryComponent
+   */
   opened = false;
 
   ngOnInit(): void {}
 
+  /**
+   * A function to open the clicked image
+   * @param {*} image
+   * @memberof GalleryComponent
+   */
   openImage(image: any) {
     this.opened = true;
     image.target.classList.add('opened');
     image.target.nextElementSibling.classList.add('show');
   }
+
+  /**
+   *A function to close the opened image
+   * @memberof GalleryComponent
+   */
   closeImage() {
     this.opened = false;
     document.getElementsByClassName('opened')[0].classList.remove('opened');
